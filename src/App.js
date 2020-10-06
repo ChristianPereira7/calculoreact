808adacd2e9fbfca8c84b72c7bfbeab7fb7645c5
Christian Pereira <christianpbsd@gmail.com>
2020-10-06 05:02:56 -0300

    Foram realizadas alteracoes nos arquivos de App.js e no stilo do layout

--- src/App.js
+++ src/App.js
@@ -1,30 +1,56 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
+import './App.css';
 
+
+const Title = styled.h1`
+    text-align: center;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 50px;
+    color: #ffffff;
+    margin-bottom:100px;
+`; 
 
 const Input = styled.input`
     width: 400px;
     height: 20px;
     font-size: 16px;
     padding: 10px;
+    border-radius: 10px;
 `;
+
+const ContainerDiv = styled.div`
+    color: #ffffff;
+    font-family: Arial, Helvetica, sans-serif;
+    font-size: 25px;
+    text-align: center;
+`;
+
 
 function App(){
 
-  const [ valorConta, setConta ] = useState('');
-  const [ valorGorjeta, setGorjeta ] = useState('');
-
+  const [ vlConta, setConta ] = useState();
+  const [ vlGorjeta, setGorjeta ] = useState();
 
     return(
-      <>
-      <div> 
-        <Input placeholder="Valor da conta" type="number" value={valorConta} onChange={(e)=>setConta(e.target.value)}/>
-        <Input placeholder="Valor da gorjeta" type="number" value={valorGorjeta} onChange={(e)=>setGorjeta(e.target.value)}/>
-      
-      
-      </div>   
-      </>
-    );
+      <ContainerDiv>
+        <Title>Calculo de Gorjeta</Title>
+        <p>Qual é a valor da conta?</p>
+        <Input placeholder="Informe o valor da conta" type="number" value={vlConta} onChange={(e)=>setConta(parseFloat(e.target.value))}/>
+        <p>Qual é o valor da gorjeta?</p>
+        <Input placeholder="Informe o valor da gorjeta" type="number" value={vlGorjeta} onChange={(e)=>setGorjeta(parseFloat(e.target.value))}/>    
+        <hr/>
+        
+       {vlConta > 0 &&
+        <>
+            <p>Sub-total: R$ {vlConta}</p>
+            <p>Gorjeta foi de ({vlGorjeta}%): R$ {(vlGorjeta/100) * vlConta}</p> 
+            <p>Total da conta: R$ {vlConta + ((vlGorjeta/100) * vlConta)}</p>
+        </>
+        }
+     </ContainerDiv>
+   );
+  
 }
 
 export default App;
